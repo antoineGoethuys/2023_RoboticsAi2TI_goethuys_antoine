e1d5427148b00c62caf4355946be9c0d55c60f04
antoineGoethuys <antoine.goethuys@student.ehb.be>
2023-11-17 10:23:31 +0100

    commit patrol

---
+++ build/patrol_pkg/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/ubuntu/ros2_ws/src/2023_RoboticsAi2TI_goethuys_antoine/install/patrol_pkg'
---
+++ install/publisher_pkg/share/publisher_pkg/publisher_pkg_launch_file.launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package='publisher_pkg',
+                executable='simple_publisher',
+                output='screen'),
+        ]
+    )
