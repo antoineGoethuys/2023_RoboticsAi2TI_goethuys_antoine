a2d700c5c2b5e277dd3a585e608d8604f8b71d45
antoineGoethuys <antoine.goethuys@student.ehb.be>
2023-12-01 18:52:11 +0100

    Update practical_test_V2.py
    all work but it dont turn right

--- practical_test_V2_pkg/practical_test_V2_pkg/practical_test_V2.py
+++ practical_test_V2_pkg/practical_test_V2_pkg/practical_test_V2.py
@@ -1,97 +1,95 @@
-import rclpy, math
-# import the ROS2 python libraries
+import rclpy
 from rclpy.node import Node
-# import the Twist module from geometry_msgs interface
-from geometry_msgs.msg import Twist
-# import the LaserScan module from sensor_msgs interface
 from sensor_msgs.msg import LaserScan
-from rclpy.qos import ReliabilityPolicy, QoSProfile
+from geometry_msgs.msg import Twist
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
 
-class  Practical_test(Node):
-
+class ObstacleAvoidanceSystem(Node):
     def __init__(self):
-        # Here you have the class constructor
-        # call the class constructor
-        super().__init__('practical_test')
-        # create the publisher object
-        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
-        # create the subscriber object
-        self.subscriber = self.create_subscription(LaserScan, '/scan', self.laser_callback, QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
-        # define the timer period for 0.5 seconds
-        self.timer_period = 1.0  # seconds
-        # define the variable to save the received info
-        self.laser_frontForward = 0
-        self.laser_frontLeft = 0
-        self.laser_frontRight = 0
-        self.laser_frontBack = 0
+        super().__init__("ObstacleAvoidanceSystem")
+        self.system_started = False
+        self.setup_subscriber_publisher()
 
-        # create a Twist message
-        self.cmd = Twist()
-        self.timer = self.create_timer(self.timer_period, self.motion)
+        self.motion_command = Twist()
+        self.turn_direction = 1
+        self.blocked_turn_direction = False
+        self.turn_speed = 0.5
 
-    def laser_callback(self,msg):
-        # Save the frontal laser scan info at 0°
-        #self.get_logger().info(f'{msg.range_min} : { msg.range_max}')
-        if msg:
-            degree = int(msg.range_max/12) # 30°
-            self.get_logger().info(f'{degree}')
-            self.laser_frontForward = msg.ranges[-1] # 0°
-            self.laser_frontRight = min(msg.ranges[0:30])          # 0° to 30°
-            self.laser_frontLeft = min(msg.ranges[-30:-1])          # -30° to 0°
-            self.laser_frontBack = min(msg.ranges[-30*5:30*5]) # -180° to 180°
+    def setup_subscriber_publisher(self):
+        qos_profile_scan = self.create_qos_profile(QoSReliabilityPolicy.BEST_EFFORT, QoSHistoryPolicy.KEEP_LAST)
+        self.subscription = self.create_subscription(LaserScan, "/scan", self.process_scan_data, qos_profile_scan)
 
+        qos_profile_cmd_vel = self.create_qos_profile(QoSReliabilityPolicy.RELIABLE, QoSHistoryPolicy.KEEP_LAST)
+        self.publisher = self.create_publisher(Twist, "/cmd_vel", qos_profile_cmd_vel)
 
-        
-    def motion(self):
-        # print the data
-        #self.get_logger().info(f'forward: {self.laser_frontForward} right: {self.laser_frontRight} left: {self.laser_frontLeft} back: {self.laser_frontBack}')
-        started = False
-        stop = False
-        # Logic of move
-        if self.laser_frontBack < 0.1:
-            started = True
-        
-        if self.laser_frontBack < 0.1 and self.laser_frontForward < 0.1:
-            stop = True
+    def create_qos_profile(self, reliability, history):
+        return QoSProfile(reliability=reliability, history=history, depth=10)
 
-        
-        if started:
-            # stop
-            if self.laser_frontBack < 0.1 and self.laser_frontForward < 0.1:
-                stop = True
-            # move forward
-            if self.laser_frontRight > 0.3 and self.laser_frontForward > 0.3 and self.laser_frontRight > 0.3:
-                self.cmd.linear.x = 0.2
-                self.cmd.angular.z = 0.0
-            # turn left
-            elif self.laser_frontRight > self.laser_frontLeft:
-                self.cmd.linear.x = 0.0
-                self.cmd.angular.z = math.radians(90)
-            # turn right
-            else:
-                self.cmd.linear.x = 0.0
-                self.cmd.angular.z = math.radians(-90)
-            # stop
-            if stop:
-                self.cmd.linear.x = 0.0
-                self.cmd.angular.z = 0.0
-                started = False
-        # Publishing the cmd_vel values to a Topic
-        self.publisher_.publish(self.cmd)
+    def process_scan_data(self, scan_msg):
+        if not self.system_started and scan_msg.ranges[0] < 0.2:
+            self.system_started = True
+            self.get_logger().info("System Started")
+        else:
+            self.process_motion(scan_msg)
 
+    def process_motion(self, scan_msg):
+        front_distances = self.extract_front_distances(scan_msg)
 
-            
+        if min(front_distances) < 0.18:
+            self.handle_obstacle()
+        else:
+            self.adjust_linear_motion(front_distances)
+
+        if min(front_distances) <= 0.18:
+            if not self.blocked_turn_direction:
+                self.check_turn_direction()
+
+            self.adjust_angular_motion()
+        else:
+            self.motion_command.angular.z = 0.0
+
+        self.publisher.publish(self.motion_command)
+
+    def extract_front_distances(self, scan_msg):
+        return scan_msg.ranges[-15:] + scan_msg.ranges[:15]
+
+    def handle_obstacle(self):
+        self.blocked_turn_direction = True
+        self.motion_command.linear.x = 0.0
+        self.get_logger().info("Stop")
+
+    def adjust_linear_motion(self, front_distances):
+        self.blocked_turn_direction = False
+        self.motion_command.linear.x = min(0.15, 0.15 * min(front_distances))
+
+    def adjust_angular_motion(self):
+        if not self.blocked_turn_direction:
+            self.check_turn_direction()
+
+        self.motion_command.angular.z = self.turn_direction * self.turn_speed
+        self.get_logger().info("Turning")
+
+    def check_turn_direction(self):
+        length_array = len(scan_msg.ranges) * 60 // 360
+        right_distances = scan_msg.ranges[0:length_array]
+        left_distances = scan_msg.ranges[-length_array:]
+        sum_right_distances = sum(right_distances)
+        sum_left_distances = sum(left_distances)
+
+        if sum_right_distances < sum_left_distances:
+            self.turn_direction = -1
+        elif sum_right_distances > sum_left_distances:
+            self.turn_direction = 1
+        else:
+            self.turn_direction = 0
+            self.get_logger().info("Turn direction: " + ("right" if self.turn_direction == -1 else "left"))
+
 def main(args=None):
-    # initialize the ROS communication
     rclpy.init(args=args)
-    # declare the node constructor
-    practical_test = Practical_test()       
-    # pause the program execution, waits for a request to kill the node (ctrl+c)
-    rclpy.spin(practical_test)
-    # Explicity destroy the node
-    practical_test.destroy_node()
-    # shutdown the ROS communication
+    obstacle_avoidance_system = ObstacleAvoidanceSystem()
+    rclpy.spin(obstacle_avoidance_system)
+    obstacle_avoidance_system.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
